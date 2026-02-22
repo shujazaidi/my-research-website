dc3ec0467e29bca0ad432384f3a502b2728dfb19
shujazaidi <65902846+shujazaidi@users.noreply.github.com>
2026-02-22 10:52:28 +0000

    Deploying to gh-pages from @ shujazaidi/my-research-website@b119bee6edc0b7e435bd5c901be8a96d1765d9d8 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://shujazaidi/my-research-website.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://shujazaidi.github.io/my-research-website//al-folio/#contact">contact us</a>.'
               }
             ]
           }
