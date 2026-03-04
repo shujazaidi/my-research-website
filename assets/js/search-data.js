179df4cbaac506c86cb3860ee517a7ddd890f97c
shujazaidi <65902846+shujazaidi@users.noreply.github.com>
2026-03-04 10:52:43 +0000

    Deploying to gh-pages from @ shujazaidi/my-research-website@3e8ad96f3a8211fccf44112ad71afd41959ea76b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,7 +429,7 @@ ninja.data = [{
             },},{id: "news-phd-awarded-in-physics-thesis-title-kinetics-of-phase-transitions-in-multicomponent-fluid-mixtures-using-computer-simulations-role-of-surface-potential-and-mixture-composition",
           title: 'PhD awarded in Physics. Thesis title: Kinetics of phase transitions in multicomponent fluid...',
           description: "",
-          section: "News",},{id: "news-our-paper-on-barrier-crossing-by-partially-active-polymers-is-published-in-j-chem-phys-https-doi-org-10-1063-5-0291163",
+          section: "News",},{id: "news-our-paper-on-barrier-crossing-by-partially-active-polymers-is-published-in-j-chem-phys",
           title: 'Our paper on barrier crossing by partially active polymers is published in J....',
           description: "",
           section: "News",},{id: "news-our-collaborative-work-on-active-colloidal-suspension-is-accepted-in-pre-as-a-regular-article-sparkles-smile",
