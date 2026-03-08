730fb62157ca0ff1e9a678a179b0734fa85f5e59
shujazaidi <65902846+shujazaidi@users.noreply.github.com>
2026-03-08 18:35:24 +0000

    Deploying to gh-pages from @ shujazaidi/my-research-website@5ba51c69716ccfa6ec6147ba1e035a62e90a03ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,21 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/my-research-website/projects/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/my-research-website/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/my-research-website/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
